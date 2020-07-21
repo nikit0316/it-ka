522e94cd742f8e779016ceb7c961bdfa824cbe0c
Nikita Treskov <treskov-1@mail.ru>
2020-07-21 15:00:47 +0300

    11 chapter

---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'><img src='https://www.zastavki.com/pictures/originals/2014/Nature___Waterfalls____Waterfall_in_the_rainforest_071165_.jpg'></img>      
+    <div>avatar+descrip</div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
