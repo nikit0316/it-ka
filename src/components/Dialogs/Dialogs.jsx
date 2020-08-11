d74577283be2a53e2bc118f5a5541fd911612f6e
Treskov Nikita <treskov-1@mail.ru>
2020-08-11 22:22:13 +0300

    css fix

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import styles from './Dialogs.modules.css';
+import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog  + ' ' + styles.active}>Dimych</div>
+                <div className={styles.dialog}>Andrey</div>
+                <div className={styles.dialog}>Sveta</div>
+                <div className={styles.dialog}>Viktor</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.dialog}>Hi</div>
+                <div className={styles.dialog}>How are you</div>
+                <div className={styles.dialog}>How's your day</div>
+                <div className={styles.dialog}></div>
+            </div>
         </div>
     )
 }
