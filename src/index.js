427b28235e14c12e836f1ac95cb1cb871527398a
Nikita Treskov <treskov-1@mail.ru>
2020-09-16 17:34:50 +0300

    OOP + bind

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {addPost, subscribe, updateNewPostText} from './redux/state';
+import store from './redux/state';
 import App from "./App";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,55 +1,55 @@
-let rerenderEntireTree =() =>{
-}
-
-
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi how are you', count: 15},
-            {id: 2, message: "It's my first post", count: 20},
-            {id: 3, message: "BlaBla", count: 25},
-            {id: 4, message: "Skyv", count: 23}
-        ],
-        newPostText: 'it-kamasutra'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi how are you', count: 15},
+                {id: 2, message: "It's my first post", count: 20},
+                {id: 3, message: "BlaBla", count: 25},
+                {id: 4, message: "Skyv", count: 23}
+            ],
+            newPostText: 'it-kamasutra'
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Dimon'},
+                {id: 2, name: 'Dimych'},
+                {id: 3, name: 'Andrey'},
+                {id: 4, name: 'Sveta'},
+                {id: 5, name: 'Sasha'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you'},
+                {id: 3, message: 'sup'}
+            ]
+        }
     },
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Dimon'},
-            {id: 2, name: 'Dimych'},
-            {id: 3, name: 'Andrey'},
-            {id: 4, name: 'Sveta'},
-            {id: 5, name: 'Sasha'}
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you'},
-            {id: 3, message: 'sup'}
-        ]
+    getState(){
+      return this._state;
+    },
+    _callSubscriber(){
+        console.log('State changed');
+    },
+    addPost(postMessage) {
+        debugger;
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            count:3
+        };
+
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText='';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText=newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber=observer;  //observer pattern
     }
 }
 
-window.state=state;
-
-export const addPost = (postMessage) => {
-    debugger;
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        count:3
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText='';
-    rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText=newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree=observer;  //observer pattern
-}
-
-export default state;
+window.store=store;
+export default store;
